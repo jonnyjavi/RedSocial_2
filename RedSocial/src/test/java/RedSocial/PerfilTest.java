34d54eaca4daf3b725b53749102b0d399cd9acdc
jonnyjavi <jonathanjavier.calvasoto@gmail.com>
2025-05-14 16:46:58 +0200

    UT5.2 CI-CD - Jonathan Javier Calva Soto

---
+++ RedSocial/src/test/java/RedSocial/PerfilTest.java
@@ -0,0 +1,35 @@
+package RedSocial;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import RedSocial.Perfil;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PerfilTest {
+    private Perfil perfil;
+
+    @BeforeEach
+    void setUp() {
+        perfil = new Perfil("TestUser");
+    }
+    
+    @Test
+    void testAgregarAmigo() {
+        perfil.agregarAmigo("Bob");
+        List<String> amigos = perfil.listaAmigos();
+        assertTrue(amigos.contains("Bob"));
+    }
+
+    @Test
+    void testEliminarAmigo() {
+        perfil.agregarAmigo("Bob");
+        perfil.eliminarAmigo("Bob");
+        List<String> amigos = perfil.listaAmigos();
+        assertFalse(amigos.contains("Bob"));
+    }
+
+
+}
